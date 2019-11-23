1b614c8829be5cf217311d732f3a7c0221300467
unknown <srvladyk555@gmail.com>
2019-11-23 10:52:06 +0200

    added getUser function

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,11 @@
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
+const userBtn = document.getElementById('users')
+const list = document.getElementById('usersList')
+
+let users
+
 function request (method, url) {
   return new Promise((resolve, reject) => {
 	  var xhr = new XMLHttpRequest()
@@ -9,4 +14,23 @@ function request (method, url) {
 	  xhr.onerror = reject
 	  xhr.send()
   })
+}
+
+userBtn.addEventListener('click', getUsers)
+
+function getUsers (e) {
+  request('GET', 'https://jsonplaceholder.typicode.com/users')
+    .then(res => {
+      users = JSON.parse(res.target.responseText)
+      for (var i = 0; i < users.length; i++) {
+      	const listEl = document.createElement('a')
+      	listEl.textContent = users[i].name
+      	listEl.setAttribute('href', '#')
+      	listEl.classList.add('list-group-item', 'list-group-item-action')
+      	list.append(listEl)
+      }
+      console.log(users)
+    }).catch(err => {
+      console.log(err)
+    })
 }
